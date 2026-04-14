ba9cb7ef9e38ba6e0f1d0e2a46efbbca681e7bc6
GlennHS <glenniumhs@gmail.com>
2026-04-14 12:34:27 +0100

    Fixes #84

--- app/constants/Constants.ts
+++ app/constants/Constants.ts
@@ -6,9 +6,9 @@ export const METRIC_HARD_FLOOR = 0 // Adjusted metrics less than this will be ca
 export const METRIC_DAMPENING_FACTOR = 0.88 // Metrics above/below soft caps will be adjusted by this factor
 
 export const METRIC_INFO_TEXT = {
-  mind: 'MIND is your physical health. This is your heart health, your "7 minute mile", your "my knee shouldn\'t make that noise" metric.',
-  body: 'BODY is your mental health. This is your mind\'s health, letting your brain relax, switching off or challenging it to grow and stimulating it (positively).',
-  cash: 'CASH is your academic/career health. This can be climbing the corporate ladder, personal projects, learning something new, studying, doing that essay you were meant to do.',
-  work: 'WORK is your financial health. This is how healthy your wallet is, boosted by things like saving and spending responsibly.',
+  mind: 'MIND is your mental health. This is your mind\'s health, letting your brain relax, switching off or challenging it to grow and stimulating it (positively).',
+  body: 'BODY is your physical health. This is your heart health, your "7 minute mile", your "my knee shouldn\'t make that noise" metric.',
+  cash: 'CASH is your financial health. This is how healthy your wallet is, boosted by things like saving and spending responsibly.',
+  work: 'WORK is your academic/career health. This can be climbing the corporate ladder, personal projects, learning something new, studying, doing that essay you were meant to do.',
   bond: 'BOND is your social health. This can be caring for your friends, making time for your family or showing your partner you care; nurturing relationships boosts this metric.',
 } as const
