58786a56f9d27330aee92fc99fb96d5d2b81bbfd
Glenn Hamilton-Smith <glenniumhs@gmail.com>
2026-04-25 18:02:47 +0100

    feat: Add analytics. Add opt-in banner. Ensure component not rendered until consent given.

---
+++ app/components/AnalyticsBanner.tsx
@@ -0,0 +1,20 @@
+interface Props {
+  consentHandler: (consentGiven: boolean) => void
+}
+
+export default function AnalyticsBanner({ consentHandler } : Props) {
+  return (
+    <div className="w-full flex items-center justify-center text-center fixed top-0 left-0 backdrop-blur-lg">
+      <div className="flex items-center justify-around gap-2 max-w-3xl w-full bg-bond/20 text-black border-b-2 border-bond shadow-2xl px-2 py-4 z-50">
+        <div className="flex flex-col items-center justify-center gap-4">
+          <span className="font-bold text-2xs">Please can I collect some anonymous usage stats? It really helps me develop FIVE!</span>
+          <span className="italic text-3xs">I use <a href="https://vercel.com/" target="_blank" className="font-bold text-bond underline">Vercel</a> for my analytics. You can see their privacy policy <a href="https://vercel.com/docs/analytics/privacy-policy" target="_blank" className="text-bond font-bold underline">here</a>.</span>
+        </div>
+        <div className="flex flex-col items-center justify-center gap-4 text-xs">
+          <button onClick={() => consentHandler(true)} className="border border-black rounded-xl bg-bond text-white font-bold active:bg-gray-300 px-4 py-1">Sure!</button>
+          <button onClick={() => consentHandler(false)} className="text-black text-2xs italic underline active:bg-gray-300 py-1">No thanks</button>
+        </div>
+      </div>
+    </div>
+  )
+}
--- app/layout.tsx
+++ app/layout.tsx
@@ -10,6 +10,8 @@ import { ToastProvider } from "./context/ToastContext";
 import { NextStep, NextStepProvider } from "nextstepjs";
 import steps from "./tour";
 import { Settings } from "./lib/settings";
+import AnalyticsBanner from "./components/AnalyticsBanner";
+import dynamic from "next/dynamic";
 
 const jakarta = Plus_Jakarta_Sans({
   weight: '400',
@@ -23,6 +25,18 @@ export default function RootLayout({
 }>) {
   const [scrollDirection, setScrollDirection] = useState<'up' | 'down' | null>(null);
   const [scrolledToTop, setScrolledToTop] = useState<boolean>(true);
+  const [loadedConsent, setLoadedConsent] = useState<boolean>(false);
+  const [givenConsent, setGivenConsent] = useState<boolean | null>(null);
+
+  const Analytics = dynamic(
+    () => import('@vercel/analytics/next').then(mod => mod.Analytics),
+    { ssr: false }
+  )
+
+  const handleConsent = (gaveConsent: boolean): void => {
+    window.cookieStore.set('analytics-consent', `${gaveConsent ? 'yes' : 'no'}`)
+    setGivenConsent(true)
+  }
 
   useEffect(() => {
     let lastY = window.scrollY
@@ -52,6 +66,13 @@ export default function RootLayout({
     }
   }, []);
 
+  useEffect(() => {
+    window.cookieStore.get('analytics-consent').then(res => {
+      if (res?.value === 'yes') setGivenConsent(true)
+      else if (res?.value === 'no') setGivenConsent(false)
+    }).then(() => setLoadedConsent(true))
+  }, [])
+
   return (
     <html lang="en" className={jakarta.className}>
       <head>
@@ -65,6 +86,7 @@ export default function RootLayout({
         <link rel="icon" href="/images/icons/five-icon-256.png" sizes="any" />
       </head>
       <body>
+        { givenConsent && <Analytics />}
         <div className="w-full flex flex-col justify-baseline items-center">
           <div className="max-w-3xl w-full p-4">
             <NextStepProvider>
@@ -76,6 +98,7 @@ export default function RootLayout({
                     onComplete={() => Settings.set('wantsTutorial', 'false')}
                     onSkip={() => Settings.set('wantsTutorial', 'false')}
                   >
+                    { givenConsent === null && loadedConsent && <AnalyticsBanner consentHandler={handleConsent} /> }
                     { children }
                   </NextStep>
                 </ToastProvider>
