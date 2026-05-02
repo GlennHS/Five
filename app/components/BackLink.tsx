c5a270325bf6f132ec796f66c4b9907cf7fd08b0
GlennHS <glenniumhs@gmail.com>
2026-05-02 16:29:29 +0100

    fix: Fix backlink being clickable on whitespace to its right.

--- app/components/BackLink.tsx
+++ app/components/BackLink.tsx
@@ -2,7 +2,7 @@ export default function BackLink() {
   return (
     <button
       aria-roledescription="link"
-      className="text-sm font-medium text-slate-500 underline underline-offset-2 hover:text-slate-700 w-full text-left"
+      className="text-sm font-medium text-slate-500 underline underline-offset-2 hover:text-slate-700 w-fit text-left"
       onClick={() => window.history.back()}
     >
       ← Back to previous page
